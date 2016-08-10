a1b17009574b6274c6ab6df4b12c7faa8420de4a
Spyro <web2@sinonrien.fr>
2016-08-10 06:28:32 +0200

    version 720p vs 360p avec un paramètre global création des masques

--- realmyop.sh
+++ realmyop.sh
@@ -14,17 +14,34 @@ mkdir -p $DIR/imgs/redx
 mkdir -p $DIR/found
 
 # convertir les videos en segments
-## ffmpeg -i videos/darksouls2.mp4 -map 0 -c copy -f segment -segment_time 60 -reset_timestamps 1 segments/vid_2_%08d.mp4
+## ffmpeg -i videos/darksouls2.mp4 -map 0 -c copy -f segment -segment_time 60 -reset_timestamps 1 segments/vid_02_%08d.mp4
 
 # Extraire des images de la vidéo. Par exemple 2 images par seconde.
+# résolution
+res=360
+MASKDIR="$res"
+if ((res == 720)); then
+	# nb dans le masque: 12801
+	MINPIXELON=2300
+	# nb dans le masque: 24999
+	MAXPIXELOFF=2500
+fi
+if ((res == 360)); then
+	# nb dans le masque: 3741
+	MINPIXELON=1500
+	# nb dans le masque: 6435
+	MAXPIXELOFF=2000
+fi
+# numéro de la vidéo (%02d)
 videoNum="03"
-vid=0
+# passer combien de vidéos au début
 startat=0
-skipatstart=((startat+vid))
+# etc.
+vid=0
 skip=0
-for segment in $DIR/segments/vid${videoNum}_*.mp4; do
+for segment in $DIR/segments/vid_${videoNum}_*.mp4; do
 	vid=$((vid+1))
-	if ((vid < skipatstart)); then
+	if ((vid < startat)); then
 		continue
 	fi
 	# supprimer les fichiers du dossier d'images
@@ -36,7 +53,7 @@ for segment in $DIR/segments/vid${videoNum}_*.mp4; do
 	mkdir -p $DIR/imgs/maskx
 	mkdir -p $DIR/imgs/redx
 	# créer les images de ce segment
-	nvid=`printf "%04d" $vid`
+	nvid=`printf "%04d" $((vid-1))`
 	ffmpeg -i "$segment" -vf fps=$FPS "$DIR/imgs/frames/death_${videoNum}_${nvid}_%04d.png"
 	
 	echo "============ "`basename $segment`" || ($vid) ============"
@@ -53,7 +70,12 @@ for segment in $DIR/segments/vid${videoNum}_*.mp4; do
 		# - croper la zone de l'écran qui doit le contenir
 		# x/y: 342x274 w/h: 420x90
 		# $ convert imgs/frames/out$x.png -crop 420x90+342+274 imgs/crops/out$x.png
-		convert "$file" -crop 420x90+342+274 "$DIR/imgs/crops/$NAME"
+		if ((res == 360)); then
+			convert "$file" -crop 212x48+171+134 "$DIR/imgs/crops/$NAME"
+		fi
+		if ((res == 720)); then
+			convert "$file" -crop 420x90+342+274 "$DIR/imgs/crops/$NAME"
+		fi
 	
 		# - appliquer un masque pour ne garder que la zone du texte
 		# $ composite -compose Multiply out67c.png mask-off.png out67m.png
@@ -74,7 +96,7 @@ for segment in $DIR/segments/vid${videoNum}_*.mp4; do
 		
 		# - comparer à un seuil
 		# Si c'est bon
-		if ((PIXELON > 2300)); then
+		if ((PIXELON > MINPIXELON)); then
 
 			# Tester si tout l'écran est rouge:
 			# - faire un masque pour la zone autour des mots "YOU DIED"
@@ -88,7 +110,7 @@ for segment in $DIR/segments/vid${videoNum}_*.mp4; do
 		
 			# - comparer le taux de rouge à celui de la zone des mots
 			# (il doit en effet y avoir un masque noir autour des mots)
-			if ((PIXELOFF > 2500)); then
+			if ((PIXELOFF > MAXPIXELOFF)); then
 				cp "$file" "$DIR/false/$NAME"
 			else
 				printf ".\n"
